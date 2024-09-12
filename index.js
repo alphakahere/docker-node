6e9305d3654857c8b66c6f5eb9535ffe3b69ac51
alphakahere <alphakahere@gmail.com>
2024-09-12 10:41:49 +0000

    config docker node app and mongo db

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const {
+	MONGO_USER,
+	MONGO_PASSWORD,
+	MONGO_IP,
+	MONGO_PORT,
+	PORT,
+} = require("./config/config");
+
+const app = express();
+
+const port = PORT;
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+	mongoose
+		.connect(mongoUrl)
+		.then(() => console.log("Successfully connected to DB"))
+		.catch((err) => {
+			console.log(err);
+			setTimeout(connectWithRetry, 5000);
+		});
+};
+
+connectWithRetry();
+
+app.get("/", (req, res) => {
+	res.send("<h1>Hello World !</h1>");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
